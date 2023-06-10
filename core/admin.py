e9231dabb059a7a654f6735b52bd6d7fe4eadf8f
stas23111 <stas-lomaev@mail.ru>
2023-06-10 16:05:23 +0400

    todolist_2

---
+++ core/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+
+from core.models import User
+
+
+# @admin.register(User)
+# class CustomUserAdmin(UserAdmin):
+#     list_display = ['username', 'email', 'first_name', 'last_name']
+#     exclude = ['groups', 'user_permissions', 'password', 'is_superuser']
+#     search_fields = ['username', 'first_name', 'last_name']
+#     list_filter = ['is_staff', 'is_active', 'is_superuser']
+#     readonly_fields = ['date_joined', 'last_login']
+#
+#
+# admin.site.unregister(Group)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name']
+    exclude = ['groups', 'user_permissions', 'password', 'is_superuser']
+    search_fields = ['username', 'first_name', 'last_name']
+    list_filter = ['is_staff', 'is_active', 'is_superuser']
+    readonly_fields = ['date_joined', 'last_login']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
